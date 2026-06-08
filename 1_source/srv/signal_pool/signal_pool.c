3c78a3572c1bafa7bbc25b5d86fc760e8e57e6fe
crodegast <c.rodegast@elobau.de>
2026-06-08 09:55:21 +0200

    🐛 [fix] 24-0115-9495 24-0115-9583 (check for array bounds)

--- 1_source/srv/signal_pool/signal_pool.c
+++ 1_source/srv/signal_pool/signal_pool.c
@@ -335,7 +335,9 @@ sp_result_t set_signal_float(const sig_nr_pools_t sig_pool, const sig_config_t r
 {
     sp_result_t ret_val = check_accessor_par(sig_pool, range, sig_nr);
 
-    if (ERR_SP_SUCCESS == ret_val)
+    if ((ERR_SP_SUCCESS == ret_val)
+        && (sig_nr < SP_NR_OF_MEM_CELLS)
+        && (sig_pool < sig_nr_max))
     {
         if (ffp32 == pool_memory[sig_pool].values[sig_nr].signal_type)
         {
